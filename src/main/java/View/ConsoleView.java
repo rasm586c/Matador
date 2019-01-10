ef6ee2b673d3effb8242d500f36670d087facd04
Rasmus <rsoeborg@gmail.com>
2019-01-10 15:48:27 +0100

    Lavet en ConsoleView som kan benyttes til debugging.

---
+++ src/main/java/View/ConsoleView.java
@@ -0,0 +1,28 @@
+package View;
+
+import Model.*;
+
+public class ConsoleView implements View {
+    public String getUserInput(String message) {
+        System.out.println(message);
+        return "";
+    }
+
+    public String getUserSelect(String message, String... args) {
+        return "";
+    }
+
+    public void printDiceRoll(int value1, int value2) {
+        System.out.println("You rolled " + value1 + " and " + value2);
+    }
+
+
+    public void movePlayer(int oldPosition, int newPosition, Player player) { }
+
+    public void print(String print) {
+        System.out.println(print);
+    }
+
+    public void updatePlayers(Player[] players) {}
+    public void updateBoard(GameBoard board) {}
+}
