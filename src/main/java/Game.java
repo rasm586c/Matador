cb6d235e5dc7cf3f2b90168c929a20e09fac193a
Rasmus <rsoeborg@gmail.com>
2019-01-10 15:48:47 +0100

    Tilføjet turnController samt simpelt opsætning af Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,22 +2,28 @@ import Controller.*;
 import Model.*;
 import View.*;
 
+import java.io.IOException;
+
 public class Game {
-    public static void main(String[] args) {
-        View view = new GUIView();
-        TurnController turnController = new TurnController(view, getPlayers());
+    public static void main(String[] args) throws IOException {
+        LanguagePackFactory languagePackFactory = new LanguagePackFactory(LanguagePackFactory.LanguageType.Danish);
+        LanguagePack languagePack = languagePackFactory.getLanguagePack();
+
+        // ...
+        View view = new GUIView(languagePack);
+        Player[] players = getPlayers(view, languagePack);
+        TurnController turnController = new TurnController(view, players, languagePack);
+        view.updatePlayers(players);
 
         // Game Loop
         while (true) {
             turnController.takeTurn();
             turnController.getNextPlayer();
-
         }
     }
 
-    private static Player[] getPlayers() {
-        return new Player[] {
-
-        };
+    private static Player[] getPlayers(View view, LanguagePack languagePack) {
+        RetrievePlayerDialog dialog = new RetrievePlayerDialog(view, languagePack);
+        return dialog.showPlayerDialog();
     }
 }
