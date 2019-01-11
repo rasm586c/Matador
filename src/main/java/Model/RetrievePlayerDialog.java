9471832b7f8e86e0e60db2c77e548cccf85c9b30
Nicklas185105 <s185105@student.dtu.dk>
2019-01-11 13:01:18 +0100

    Har ændret fra 2-4 spillere til 3-6 spillere

--- src/main/java/Model/RetrievePlayerDialog.java
+++ src/main/java/Model/RetrievePlayerDialog.java
@@ -22,7 +22,7 @@ public class RetrievePlayerDialog {
     private Player[] getPlayers() {
         int n;
 
-        while ((n = Integer.parseInt(view.getUserSelect(stringContainer.getString("amount_players"), "2", "3", "4"))) <= 0) {
+        while ((n = Integer.parseInt(view.getUserSelect(stringContainer.getString("amount_players"), "3", "4", "5", "6"))) <= 0) {
             view.print(stringContainer.getString("invalid_amount_players"));
         }
 
