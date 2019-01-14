108a266a993e0c45b71929026072744200726129
Anton <s170429@student.dtu.dk>
2019-01-14 15:16:21 +0100

    Implementeret ProcessTransaction og en funktion så man kan trække penge fra spilleren

--- src/main/java/Controller/BankController.java
+++ src/main/java/Controller/BankController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Model.*;
+import Model.Fields.Field;
 import View.View;
 
 public class BankController extends Controller {
@@ -30,6 +31,13 @@ public class BankController extends Controller {
         updateBalances();
     }
 
+    public void withdrawMoney(Player player, int money) {
+        Account account = findAccountByPlayer(player);
+        account.setBalance(account.getBalance() - money);
+
+        updateBalances();
+    }
+
     private Account findAccountByPlayer(Player player) {
         for (int i = 0; i < accounts.length; i++) {
             if (accounts[i].owner == player) { return accounts[i]; }
@@ -40,6 +48,28 @@ public class BankController extends Controller {
     private void updateBalances() {
         for (int i = 0; i < accounts.length; i++) {
             view.setPlayerBalance(accounts[i].owner, accounts[i].getBalance());
+        }
+    }
+
+    public void processTransaction(Transaction transaction, GameState state) {
+        Account account = findAccountByPlayer(transaction.getPlayer());
+
+        Field landedOn = state.getBoard().getFields()[transaction.getPlayer().getPositionClamped()];
+        if(account.getBalance() >= landedOn.value) {
+            transaction.setApproved(true);
+        }
+        else {
+            transaction.setApproved(false);
+        }
+
+        if (transaction.isApproved()) {
+            landedOn.setOwner(state.getCurrentPlayer());
+            view.updateOwner(state.getCurrentPlayer(), state.getCurrentPlayer().getPositionClamped());
+
+            withdrawMoney(state.getCurrentPlayer(),landedOn.value);
+            updateBalances();
+        } else {
+            //TODO: Mere logik
         }
     }
 }
