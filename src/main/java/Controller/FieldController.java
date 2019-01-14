c911c66a4c253637b2108048006c1045577205e7
Anton <s170429@student.dtu.dk>
2019-01-14 15:17:02 +0100

    Gjort sådan at FieldController retunerer Transaction

--- src/main/java/Controller/FieldController.java
+++ src/main/java/Controller/FieldController.java
@@ -1,27 +1,40 @@
 package Controller;
 
 import Model.*;
-import Model.Fields.Field;
-import Model.Fields.PropertyField;
-import View.View;
+import Model.Fields.*;
+import View.*;
 
 public class FieldController extends Controller {
     GameBoard board;
+    View view;
 
     public FieldController(View view, GameBoard board) {
         super(view);
         this.board = board;
+        this.view = view;
     }
 
-    public void update(GameState state) {
+    public Transaction update(GameState state) {
+        Transaction transaction = null;
+
         // What field did we land on?
        Field landedOn = board.getFields()[state.getCurrentPlayer().getPositionClamped()];
        landedOn.onFieldLand(state);
 
        // Property Field
-       if (landedOn instanceof PropertyField) {
+       if (landedOn instanceof OwnableField) {
+            if (landedOn.getOwner() == null) {
+                // TODO: Fix gamestrings
+                String result = view.getUserSelect(((OwnableField)landedOn).getPurchaseText(),"yes","no") ;
 
+                if (result.equals("yes")) {
+                    transaction = new Transaction(state.getCurrentPlayer(), landedOn);
+                }
+            }
        }
+
+
+        return transaction;
     }
 
 
