69d7e6e3d2ef6613115b7e7d00a337524b73fb19
Hans Krogh <hanskroght@gmail.com>
2019-01-21 10:10:22 +0100

    Sat before op.

--- Test/Model/TransactionTest.java
+++ Test/Model/TransactionTest.java
@@ -1,6 +1,9 @@
 package Model;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -8,35 +11,69 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
+    Player player;
+    Account account;
+    LanguagePack languagePack;
+    GameBoard gameBoard;
+
+    @BeforeEach
+    void setup() {
+        player = new Player("hans", PlayerType.UFO, Color.BLACK);
+        account = new Account(player, 30000);
+
+        languagePack = new LanguagePack();
+        gameBoard = new GameBoard(languagePack);
+
+    }
 
     @Test
-    void setAmount() {
-        Player player = new Player("hans", PlayerType.UFO, Color.BLACK);
-        Account account = new Account(player, 30000);
-
-        LanguagePack languagePack = new LanguagePack();
-        GameBoard gameBoard = new GameBoard(languagePack);
-
+    void testAmount() {
         Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
 
         transaction.setAmount(1200);
         Assert.assertEquals(1200, transaction.getAmount());
+    }
+    @Test
+    void testTarget() {
+        Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
+
 
         transaction.setTarget(player);
         Assert.assertEquals(player, transaction.getTarget());
 
+    }
+    @Test
+    void testApproved(){
+        Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
+
         transaction.setApproved(true);
         Assert.assertTrue(transaction.isApproved());
+
+    }
+    @Test
+    void setplayerTest(){
+        Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
 
         transaction.setPlayer(player);
         Assert.assertEquals(player, transaction.getPlayer());
 
+    }
+
+    @Test
+    void testField() {
+        Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
+
         transaction.setField(gameBoard.getFields()[3]);
         Assert.assertEquals(gameBoard.getFields()[3], transaction.getField());
+
+    }
+    @Test
+    void TransactionType() {
+        Transaction transaction = new Transaction(player, gameBoard.getFields()[3], 1200, Transaction.TransactionType.PayToBank);
 
         transaction.setTransactionType(Transaction.TransactionType.PurchaseProperty);
         Assert.assertEquals(Transaction.TransactionType.PurchaseProperty, transaction.getTransactionType());
 
-        transaction.setField(gameBoard.getFields()[3]);
     }
+
 }
